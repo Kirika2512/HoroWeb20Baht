ea08c88f9e280d94c7447382fb9cd4976be468ad
Kirika <kirikakirito24@gmail.com>
2022-09-02 18:28:20 +0700

    Add file

---
+++ function.js
@@ -0,0 +1,12 @@
+tarot = ['The Fool', 'The Magician', 'The High Priestess', 'The Empress', 'The Emperor', 'The Hierophant', 'The Lover', 'The Chariot', 'The Strength', 'The Hermit', 'Wheel of Fortune', 'The Justice', 'The Hanged Man', 'Death', 'Temperance', 'The Devil', 'The Star', 'he Moon', 'The Sun', 'Judgement', 'The World']
+
+
+//just pick one tarot
+function randomtarot() {
+    
+}
+
+//calculate from birthday
+function personalcard() {
+
+}
